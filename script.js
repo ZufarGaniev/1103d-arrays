d4f6dd89c1305463324a2742adc0b262626f53da
ZufarGaniev <123280212+ZufarGaniev@users.noreply.github.com>
2023-11-08 10:55:05 +0000

    function arraySize, reverse and cleanArray

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 // задача №1
 const fill = (arraySize, value) => {
-  // решение здесь
+  const newArray = [];
+  for (let i = 0; i < arraySize; i++) {
+    newArray.push(value);
+  }
+  return newArray;
 }
 
-const data = 3;
+const data1 = 3;
 const valueToFill = 'a';
-console.log(fill(data, valueToFill))
+console.log(fill(data1, valueToFill))
 
 // задача №2
 const reverse = (array) => {
-  // решение здесь
- }
+  let reversedArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversedArray.push(array[i]);
+  }
+  return reversedArray;
+}
 
-const data = [1, 2, 3];
-console.log(reverse(data)); // [3, 2, 1]
+const data2 = [1, 2, 3];
+console.log(reverse(data2)); // [3, 2, 1]
 
 // задача №3
 const compact = (array) => {
-  // решение здесь
+  let cleanArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i])
+      cleanArray.push(array[i]);
+  }
+  return cleanArray;
 }
 
-const data = [0, 1, false, 2, undefined, '', 3, null];
-console.log(compact(data)) // [1, 2, 3]
+const data3 = [0, 1, false, 2, undefined, '', 3, null];
+console.log(compact(data3)) // [1, 2, 3]
